8118de6c460077a317707a79fc22b6a3bcc36d4d
Sosuke1019 <kato.052wazime@gmail.com>
2023-10-29 09:23:56 +0900

    FetchingDataコンポーネント作成

---
+++ src/views/FetchingData.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Latest Vue Core Commits</h1>
+    <template v-for="branch in branches">
+        <input type="radio"
+        :id="branch"
+        :value="branch"
+        name="branch"
+        v-model="currentBranch">
+        <label :for="branch">{{ branch }}</label>
+    </template>
+    <p>vuejs/vue@{{ currentBranch }}</p>
+</template>
+
+<script setup lang="ts">
+    import {ref, watchEffect} from 'vue'
+
+    const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`
+    const branches = ['main', 'v2-compat']
+
+    const currentBranch = ref(branches[0])
+    const commits = ref(null)
+</script>
+
+<style>
+
+</style>
