370d7444f2ccc97124b3e4e03190e3e34f3f3250
Sosuke1019 <kato.052wazime@gmail.com>
2023-10-29 09:23:03 +0900

    simpleComponentコンポーネント作成

---
+++ src/views/simpleComponent/SimpleComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <ol>
+        <TodoItem
+        v-for="item in groceryList"
+        :todo="item"
+        :key="item.id"
+        ></TodoItem>
+    </ol>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import TodoItem from './TodoItem.vue'
+
+const groceryList = ref([
+    { id: 0, text: 'Vegetables' },
+    { id: 1, text: 'Cheese' },
+    { id: 2, text: 'Whatever else humans are supposed to eat' }
+])
+</script>
